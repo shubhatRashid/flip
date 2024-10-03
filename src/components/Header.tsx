a1a2702b276bab9053210a608ef1ceb84c8583c4
shubhat33 <rashidshubhat41@gmail.com>
2024-10-03 17:09:58 +0530

    develop closing button on countdown modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 'use client'
-import { useState } from "react";
-import left from "../../assets/left.png"
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 export default function Header() {
@@ -23,6 +22,7 @@ export default function Header() {
     }
     return (
         <div className="flex w-full justify-between h-[6%] my-3 px-5">
+            
             <div className="flex justify-center items-center bg-neutral-800 rounded-xl w-[10%]">
                 <Link href='/'>FlipClock</Link>
             </div>
@@ -66,43 +66,54 @@ export default function Header() {
                                bg-neutral-700 flex flex-col justify-around items-center gap-10 
                                p-10 rounded-xl"
                 >   
-                <div className="flex justify-center items-center w-full h-full gap-10">
-                    <div className="flex flex-col h-full w-[30%] rounded-xl justify-center items-center">
-                        <label> HOURS</label>
-                        <input 
-                            type="number"  
-                            className="flex w-full h-full rounded-xl justify-center items-center text-9xl
-                                    bg-neutral-800"
-                            value={hours}
-                            onChange={(event) => setHours((event.target.value))}
-                        />
+                    <div 
+                        className="absolute top-3 right-3 rounded-full p-1 w-[25px] 
+                                    h-[25px] border flex justify-center items-center"
+                    >
+                        <button 
+                            className="text-red-500 font-sans w-full h-full flex justify-center items-center"
+                            onClick = {() => setShowCountDown(false)}
+                        >
+                                x
+                        </button>
                     </div>
-                    <div className="flex flex-col h-full w-[30%] rounded-xl justify-center items-center">
-                        <label> MINUTES</label>
-                        <input 
-                            type="number" 
-                            className="flex w-full h-full rounded-xl justify-center items-center text-9xl
-                                    bg-neutral-800"
-                            value={minutes}
-                            onChange={(event) => setMinutes((event.target.value))}
-                        />
+                    <div className="flex justify-center items-center w-full h-full gap-10">
+                        <div className="flex flex-col h-full w-[30%] rounded-xl justify-center items-center">
+                            <label> HOURS</label>
+                            <input 
+                                type="number"  
+                                className="flex w-full h-full rounded-xl justify-center items-center text-9xl
+                                        bg-neutral-800"
+                                value={hours}
+                                onChange={(event) => setHours((event.target.value))}
+                            />
+                        </div>
+                        <div className="flex flex-col h-full w-[30%] rounded-xl justify-center items-center">
+                            <label> MINUTES</label>
+                            <input 
+                                type="number" 
+                                className="flex w-full h-full rounded-xl justify-center items-center text-9xl
+                                        bg-neutral-800"
+                                value={minutes}
+                                onChange={(event) => setMinutes((event.target.value))}
+                            />
+                        </div>
+                        <div className="flex flex-col h-full w-[30%] rounded-xl justify-center items-center">
+                            <label> SECONDS</label>
+                            <input 
+                                type="number"
+                                className="flex w-full h-full rounded-xl justify-center items-center text-9xl
+                                        bg-neutral-800"
+                                value={seconds}
+                                onChange={(event) => setSeconds((event.target.value))}
+                            />
+                        </div>
                     </div>
-                    <div className="flex flex-col h-full w-[30%] rounded-xl justify-center items-center">
-                        <label> SECONDS</label>
-                        <input 
-                            type="number"
-                            className="flex w-full h-full rounded-xl justify-center items-center text-9xl
-                                    bg-neutral-800"
-                            value={seconds}
-                            onChange={(event) => setSeconds((event.target.value))}
-                        />
+                    <div className="flex w-full justify-center items-center rounded-xl border p-3 bg-neutral-800">
+                        <button className="font-bold text-2xl font-serif" onClick={handleStart}>
+                            Start 
+                        </button>
                     </div>
-                </div>
-                <div className="flex w-full justify-center items-center rounded-xl border p-3 bg-neutral-800">
-                    <button className="font-bold text-2xl font-serif" onClick={handleStart}>
-                        Start 
-                    </button>
-                </div>
                 </div>
             }
             
