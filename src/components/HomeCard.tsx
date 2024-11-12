d1c7c7f85cd106031bfe55305e4beb8c7362022b
shubhat33 <rashidshubhat41@gmail.com>
2024-11-12 11:31:54 +0530

    ui changes

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -5,7 +5,7 @@ export default function HomeCard({currImage,name,property}:{currImage:{src:strin
     return (
         <button 
             className="flex flex-col gap-3 min-w-[120px] min-h-[150px] justify-center items-center border p-2 rounded-xl 
-                        font-bold font-serif capitalise"
+                        font-bold font-mono capitalise shadow-md hover:shadow-xl hover:bg-gray-50"
             onClick={typeof property === 'string' ?() => router.push(property): () => property(true)}>
             <h2>{name}</h2>
             <Image src={currImage.src} alt="time" width={60} height={60} />
