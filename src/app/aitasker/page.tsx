9263389efbc7e9537db1efc6c6c12ae43bacc2a5
shubhat33 <rashidshubhat41@gmail.com>
2025-04-17 20:03:51 +0530

    used grok ai for task generation and displayed the tasks on frontend

--- src/app/aitasker/page.tsx
+++ src/app/aitasker/page.tsx
@@ -1,28 +1,35 @@
 'use client'
 import { useState } from "react"
-import { Check, Clipboard, LoaderCircle } from "lucide-react"
+import { Check, Clipboard, Cross, LoaderCircle, RotateCcw } from "lucide-react"
 import { Loader, SignInPage } from "@/components"
 import { useSession } from "next-auth/react"
 import { useToast } from "@/hooks/use-toast"
+import { TaskType } from "@/types"
 
 const aitasker = () => {
   const [prompt,setPrompt] = useState('')
-  const [result,setResult] = useState(false)
+  const [result,setResult] = useState(null)
   const [dataLoading,setDataLoading] = useState(false)
   const {toast} = useToast()
 
-  const handleForm = (e) => {
+  const handleForm = async (e) => {
     e.preventDefault()
     setDataLoading(true)
-    setTimeout(() => {
-      setDataLoading(false)
-      setResult(true)
-      const toastBody = {
-        title : 'Tasks generated' ,
-        description : "Your work has been broken down sucessfully",
-      }
+    const response = await fetch('/api/aitasker',{
+      method : 'POST',
+      body : JSON.stringify({prompt:prompt})
+    })
+    const jsonData = await response.json()
+    const parsedData = JSON.parse(jsonData.tasks.choices[0].message.content)
+    setResult(parsedData.tasks)
+    setDataLoading(false)
+    
+    const toastBody = {
+      title : 'Tasks generated' ,
+      description : "Your work has been broken down sucessfully",
+    }
     toast(toastBody)
-    },2000)
+
   }
   const handleApprove = () => {
     const toastBody = {
@@ -45,6 +52,7 @@ const aitasker = () => {
   return (
     <div className="w-full flex flex-col justify-center items-center gap-5 bg-gray-50 min-h-screen">
     
+      {!result && 
       <div className="w-full h-full flex flex-col justify-center items-center gap-5">
         <h1 className="text-4xl font-bold ">AI Task Breakdown</h1>
         <p className="text-gray-600">Enter your work item and let AI break it down into actionable tasks</p>
@@ -68,6 +76,7 @@ const aitasker = () => {
             </form>
         </div>
       </div>
+      }
 
       {dataLoading && <LoaderCircle className="animate-spin" size={32}/>}
 
@@ -83,20 +92,35 @@ const aitasker = () => {
             <h1 className="text-lg font-medium">Tasks</h1>
             <ul className="p-5 w-full flex flex-col gap-2">
               
-              <li className="bg-gray-100 rounded-lg w-full flex gap-3 justify-start items-center p-2">
-                <span className="rounded-full bg-green-200 p-1 w-[35px] flex justify-center items-center">01</span>
-                <span>Research Market Competitors</span>
-              </li>
+              {
+                result.map((eachTask:TaskType,index:number) => (
+                  <li key={index} className="bg-gray-100 rounded-lg w-full flex gap-3 justify-start items-center p-2">
+                    <span className="rounded-full bg-green-200 p-1 w-[35px] flex justify-center items-center">{index+1}</span>
+                    <span>{eachTask.task}</span>
+                  </li>
+                ))
+              }
         
             </ul>
+            
+            <div className="flex justify-between items-center w-full">
+              
+              <button 
+                className="text-black flex justify-center items-center  bg-gray-100 p-2 rounded-lg border"
+                onClick={() => setResult(null)}
+              > 
+                <RotateCcw  className="mx-2" size={20}/>
+                regenerate
+              </button>
 
-            <button 
-              className="text-white flex justify-center items-center ml-auto bg-green-500 p-2 rounded-lg"
-              onClick={handleApprove}
-            > 
-              <Check className="mx-2" size={20}/>
-              Approve
-            </button>
+              <button 
+                className="text-white flex justify-center items-center bg-green-500 p-2 rounded-lg"
+                onClick={handleApprove}
+              > 
+                <Check className="mx-2" size={20}/>
+                Approve
+              </button>
+            </div>
           </div>
         </div>
       }
--- src/utils/taskGenerator/taskGenerator.ts
+++ src/utils/taskGenerator/taskGenerator.ts
@@ -1,28 +1,24 @@
 const HF_API_KEY = process.env.HF_API_KEY!;
-
 export default async function generateTasks(prompt: string) {
   try {
-    const response = await fetch(
-      "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.1",
-      {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${HF_API_KEY}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          inputs: `You are a productivity assistant that breaks down work into a to-do list. Break the following into tasks.: ${prompt}`,
-        }),
-      }
-    );
-
-    const data = await response.json();
-
-    const generated = data?.[0]?.generated_text;
-
-    return {
-      tasks: generated || "Sorry, couldn't generate tasks.",
-    };
+    const response = await fetch('https://api.groq.com/openai/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${process.env.GROQ_API_KEY}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        model: 'llama3-70b-8192',
+        messages: [
+          { role: 'system', 
+            content: "You are a productivity assistant that breaks down work into a to-do list and returns the result only and nothing else the result should be in stringified JSON format which can be parsed to json. Use the following JSON structure: { \"category\": \"string - category or topic of the task\", \"tasks\": [ { \"task\": \"string - individual to-do item\", \"completed\": false",
+          },
+          { role: 'user', content: prompt}
+        ]
+      }),
+    });
+    const data = await response.json()
+    return data
   } catch (error) {
     console.error("Error generating tasks:", error);
     return {
