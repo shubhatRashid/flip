209d7c660744a6abd73600ba9a9d69943df23dc4
shubhat33 <rashidshubhat41@gmail.com>
2025-04-26 16:06:13 +0530

    changed loader to a minimal one and added lazy loading and loader to robot element

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,24 +1,10 @@
+import { LoaderPinwheel } from "lucide-react";
 import "./styles.css"
 
 export default function Loader() {
     return (
-        <div aria-label="Orange and tan hamster running in a metal wheel" role="img" className="wheel-and-hamster">
-            <div className="wheel"></div>
-            <div className="hamster">
-                <div className="hamster__body">
-                    <div className="hamster__head">
-                        <div className="hamster__ear"></div>
-                        <div className="hamster__eye"></div>
-                        <div className="hamster__nose"></div>
-                    </div>
-                    <div className="hamster__limb hamster__limb--fr"></div>
-                    <div className="hamster__limb hamster__limb--fl"></div>
-                    <div className="hamster__limb hamster__limb--br"></div>
-                    <div className="hamster__limb hamster__limb--bl"></div>
-                    <div className="hamster__tail"></div>
-                </div>
-            </div>
-            <div className="spoke"></div>
+        <div className="animate-spin">
+            <LoaderPinwheel size={35}/>
         </div>
     );
 }
--- src/components/SplineRenderer.tsx
+++ src/components/SplineRenderer.tsx
@@ -1,11 +1,16 @@
 // components/SplineRenderer.tsx
 'use client';
 
-import Spline from '@splinetool/react-spline';
+import { lazy, Suspense } from 'react';
+import Loader from './Loader/Loader';
 
 export default function SplineRenderer() {
+  const Spline = lazy(() => import('@splinetool/react-spline'))
   return (
-    <Spline scene="https://prod.spline.design/DTwMGgGM0o-kbgYx/scene.splinecode"/>
+    <Suspense fallback={<Loader/>}>
+      <Spline scene="https://prod.spline.design/DTwMGgGM0o-kbgYx/scene.splinecode"/>
+    </Suspense>
+    
   );
 }
 
